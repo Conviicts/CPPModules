e43701839a5152c4c0a4b4587ff3bfb9e8b2505b
jode-vri <jode-vri@student.42.fr>
2022-01-03 09:10:37 +0100

    42 header

--- 01/ex03/HumanA.cpp
+++ 01/ex03/HumanA.cpp
@@ -1,12 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jode-vri <jode-vri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/03 09:09:13 by jode-vri          #+#    #+#             */
+/*   Updated: 2022/01/03 09:09:14 by jode-vri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "HumanA.hpp"
 #include <iostream>
 
 HumanA::HumanA(std::string name, Weapon &weapon) : _name(name), _weapon(weapon) {
+	return ;
+}
 
-}
 HumanA::~HumanA(void) {
-	
+	return ;
 }
+
 void HumanA::attack() {
 	std::cout << _name << " attack with his " << _weapon.getType() << std::endl;
 	return ;	
