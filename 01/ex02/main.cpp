7e684a34be905d8a18f8162661971e4b79905bb0
jode-vri <jode-vri@student.42.fr>
2022-01-17 00:57:34 +0000

    .

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -20,10 +20,10 @@ int main(void)
 	std::string	&stringREF = str;
 
 	std::cout << "str address: " << &str << std::endl;
-	std::cout << "ptr address: " <<stringPTR << std::endl;
-	std::cout << "ref address: " <<&stringREF << std::endl;
+	std::cout << "ptr address: " << stringPTR << std::endl;
+	std::cout << "ref address: " << &stringREF << std::endl;
 
-	std::cout << "ptr value: " <<*stringPTR << std::endl;
-	std::cout << "ref value: " <<stringREF << std::endl;
+	std::cout << "ptr value: " << *stringPTR << std::endl;
+	std::cout << "ref value: " << stringREF << std::endl;
 	
 }
