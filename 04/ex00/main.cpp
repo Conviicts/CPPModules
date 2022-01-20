00d6ae3788583f990369c2b05a89e0d1faa44ca5
jode-vri <jode-vri@student.42.fr>
2022-01-20 03:03:49 +0100

    add more tests

--- 04/ex00/main.cpp
+++ 04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jode-vri <jode-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 09:20:07 by jode-vri          #+#    #+#             */
-/*   Updated: 2022/01/04 07:57:30 by jode-vri         ###   ########.fr       */
+/*   Updated: 2022/01/20 02:14:48 by jode-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,44 @@
 #include "Cat.hpp"
 
 int main(void) {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
+	std::cout << std::endl << "------------- CREATION -------------" << std::endl;
+	Animal			*animal = new Animal();
+	Animal			*dog = new Dog();
+	Animal			*cat = new Cat();
+	WrongAnimal 	*wrongAnimal = new WrongAnimal();
+	WrongAnimal 	*wrongCat = new WrongCat();
 
-	const WrongAnimal* meta2 = new WrongAnimal();
-	const WrongAnimal* j2 = new WrongCat();
+	std::cout << std::endl << "-------------- ANIMAL --------------" << std::endl;
+	std::cout << "Animal type: " << animal->getType() << std::endl;
+	std::cout << "Animal sound: ";
+	animal->makeSound();
 
-	std::cout << j2->getType() << " " << std::endl;
-	j2->makeSound();
-	meta2->makeSound();
+	std::cout << std::endl << "--------------- CAT ----------------" << std::endl;
+	std::cout << "Cat type: " << cat->getType() << std::endl;
+	std::cout << "Cat sound: ";
+	cat->makeSound();
+
+	std::cout << std::endl << "--------------- DOG ----------------" << std::endl;
+	std::cout << "Dog type: " << dog->getType() << std::endl;
+	std::cout << "Dog sound: ";
+	dog->makeSound();
+
+	std::cout << std::endl << "----------- WRONG ANIMAL -----------" << std::endl;
+	std::cout << "Wrong animal type: " << wrongAnimal->getType() << std::endl;
+	std::cout << "Wrong animal sound: ";
+	wrongAnimal->makeSound();
+
+	std::cout << std::endl << "------------- WRONG CAT ------------" << std::endl;
+	std::cout << "Wrong cat type: " << wrongCat->getType() << std::endl;
+	std::cout << "Wrong cat sound: ";
+	wrongCat->makeSound();
+
+	std::cout << std::endl << "-------------- DELETE --------------" << std::endl;
+	delete animal;
+	delete dog;
+	delete cat;
+	delete wrongAnimal;
+	delete wrongCat;
+
+	return (0);
 }
