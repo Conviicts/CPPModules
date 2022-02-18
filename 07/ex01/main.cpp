9d4d5e6907c3779df083fc5adf99ed204371d4fe
jode-vri <jode-vri@student.42.fr>
2022-02-18 23:57:47 +0100

    ex01

---
+++ 07/ex01/main.cpp
@@ -0,0 +1,17 @@
+#include "iter.hpp"
+
+int main( void ) {
+	int			array1[4] = {1, 2, 3, 4};
+	float   	array2[4] = {1.1, 2.2, 3.3, 4.4};
+	char		array4[4] = {'q', 'w', 'e', 'r'};
+	std::string	array3[4] = {"str1", "str2", "str3", "str4"};
+
+	iter(array1, 4, print);
+	std::cout << "----------------" << std::endl;
+	iter(array2, 4, print);
+	std::cout << "----------------" << std::endl;
+	iter(array4, 4, print);
+	std::cout << "----------------" << std::endl;
+	iter(array3, 4, print);
+	std::cout << "----------------" << std::endl;
+}
